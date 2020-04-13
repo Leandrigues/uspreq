82a025f54a2713485627c9506a87795d439feb0c
João Daniel <jotaf.daniel@gmail.com>
2020-04-13 19:17:59 -0300

    chore: init repo
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import * as bodyparser from "koa-bodyparser";
+import * as logger from "koa-logger";
+import * as json from "koa-json";
+// import {} from 'koa-cors';
+
+const app: Koa = new Koa();
+
+app.use(bodyparser());
+app.use(logger());
+app.use(json());
+
+const router: Router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.body = { msg: "Hello, world!" };
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log("server running");
+});
