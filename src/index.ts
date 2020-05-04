359eb2744e40aec3bbeb3895d43a0f951041245a
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-05-04 20:03:52 -0300

    add new routes

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,47 @@ app.use(json());
 
 const router: Router = new Router();
 
-router.get('/requisitos', (ctx) => {
-  ctx.body = { msg: `Requisitos com query: ${ctx.query.term} ${ctx.query.depth}` };
+const subject1 = {
+  cod: 'ED2',
+  pre: 'ED1',
+};
+
+const subject2 = {
+  cod: 'ED1',
+  pre: 'Intro',
+};
+
+const subjects = [subject1, subject2];
+
+const f = (subjectCod: any, a: any) => {
+  let i: number = 0;
+  while (i < 2 && subjects[i].cod != subjectCod) {
+    i++;
+  }
+  if (i != 2) {
+    a.push(subjects[i].pre);
+    f(subjects[i].pre, a); // arrumar pra varios pre requesitos
+  }
+};
+
+router.get('/requisitos', ctx => {
+  const term = ctx.query.term;
+  let a: any = [];
+  f(term, a);
+  ctx.body = { msg: `Materia: ${ctx.query.term}, pre: ${a}` };
 });
 
-router.get('/materias/:id', (ctx) => {
+router.get('/materias/:id', ctx => {
   ctx.body = { msg: `Matéria: ${ctx.params.id}` };
 });
 
+router.post('/materias', ctx => {
+  ctx.body = { msg: `Materias inseridas` };
+});
+
+router.put('/materias/:id', ctx => {
+  ctx.body = { msg: `Materia editada` };
+});
 
 app.use(router.routes()).use(router.allowedMethods());
 
