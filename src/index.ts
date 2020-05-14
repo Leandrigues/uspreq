959affeb2ddb191b742a1f0e2a641b823af45723
Leandro Rodrigues <leandro.rodrigues.silva99@gmail.com>
2020-05-14 19:31:23 -0300

    Creates subject insertion

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as logger from 'koa-logger';
 import * as json from 'koa-json';
 import { client } from './client';
 import { createTables } from './createTables';
+import { runInNewContext } from 'vm';
 
 async function initializeDB() {
   try {
@@ -37,7 +38,7 @@ const router: Router = new Router();
 
 // const f = (subjectCod: any, a: any) => {
 //   let i: number = 0;
-//   while (i < 2 && subjects[i].cod != subjectCod) {
+//   while (i < 2 || subjects[i].cod != subjectCod) {
 //     i++;
 //   }
 //   if (i != 2) {
@@ -46,31 +47,83 @@ const router: Router = new Router();
 //   }
 // };
 
-router.get('/', async (ctx) => {
-  const { db }: any = ctx;
-  await db.query(`insert into disciplinas(codigo, nome, creditos_aula, creditos_trab, periodo_ideal, link)
-  values ('MAC0323', 'Blabla', 0, 0, '0', '')
-  `);
-  const response = await db.query(`select * from disciplinas`);
-  ctx.body = { rows: response.rows };
-});
+// router.get('/', async (ctx) => {
+  // const { db }: any = ctx;
+  // await db.query(`insert into disciplinas(codigo, nome, creditos_aula, creditos_trab, periodo_ideal, link)
+  // values ('MAC0323', 'Blabla', 0, 0, '0', '')
+  // `);
+  // const response = await db.query(`select * from disciplinas`);
+  // ctx.body = { rows: response.rows };
+// });
+
+function isValid(materia : any, ctx : Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>> ) : boolean {
+  const response = {
+    error: ''
+  }
+
+  if(!materia.codigo || !materia.nome || !materia.creditos_aula || !materia.creditos_trab || !materia.periodo_ideal || !materia.link) {
+    response.error = "Matéria deve seguir o formato: {codigo, nome, creditos_aula, creditos_trab, periodo_ideal, link}";
+    return false;
+  }
+
+  if(materia.nome.length > 255) {
+    response.error = "Nome de matéria inválido";
+    return false;
+  }
+
+  if(materia.codigo.length != 7) {
+    response.error = "Código de código inválido";
+    return false;
+  }
+
+  if(typeof(materia.creditos_aula) != "number") {
+    response.error = "Créditos aula inválido";
+    return false;
+  }  
+
+  if(typeof(materia.creditos_trabalho) != "number") {
+    response.error = "Créditos trabalho inválido";
+    return false;
+  }  
+
+  if(typeof(materia.periodo_ideal) != "string" || materia.periodo_ideal.length > 255) {
+    response.error = "Perído ideal inválido";
+    return false;
+  }
+
+  if(typeof(materia.link) != "string" || materia.link.length > 255) {
+    response.error = "Link ideal inválido";
+  }
+
+  ctx.body = response;
+
+  return true;
+}
 
 router.get('/requisitos', (ctx) => {
   const { db }: any = ctx;
   const term = ctx.query.term;
   ctx.body = { db };
-  // let a: any = [];
-  // f(term, a);
-  // ctx.body = { msg: `Materia: ${ctx.query.term}, pre: ${a}` };
-  // ctx.body = res;
 });
 
 router.get('/materias/:id', (ctx) => {
   ctx.body = { msg: `Matéria: ${ctx.params.id}` };
 });
 
-router.post('/materias', (ctx) => {
-  ctx.body = { msg: `Materias inseridas` };
+router.post('/materias', async (ctx) => {
+  const { db }: any = ctx;
+  const materia = ctx.request.body;
+
+  if(isValid(materia, ctx)) {
+    const codigo = materia.codigo;
+    const nome = materia.nome;
+    const creditos_aula = materia.creditos_aula;
+    const creditos_trab = materia.creditos_trab;
+    const periodo_ideal = materia.periodo_ideal;
+    const link = materia.link;
+    
+    await db.query(`insert into disciplinas(codigo, nome, creditos_aula, creditos_trab, periodo_ideal, link)`);
+  }
 });
 
 router.put('/materias/:id', (ctx) => {
